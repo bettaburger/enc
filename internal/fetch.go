7f7846aab67252fa3896235d709b2d240082a843
stephanie <stephanieha2021@gmail.com>
2025-11-26 20:52:27 -0500

    tls for certificates and expiration dates, created Transport for more room on network performance

--- internal/fetch.go
+++ internal/fetch.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"time"
 )
 
 func hasHTTP(url string) string {
@@ -15,12 +16,46 @@ func hasHTTP(url string) string {
 	return url
 }
 
+func printTLSInfo(resp *http.Response) {
+    if resp.TLS == nil {
+        fmt.Println("No TLS (connection was HTTP)")
+        return
+    }
+
+    cs := resp.TLS.CipherSuite
+    ver := resp.TLS.Version
+    cert := resp.TLS.PeerCertificates[0]
+
+    fmt.Println("=== TLS Information ===")
+    fmt.Printf("Version: %x\n", ver)
+    fmt.Printf("Cipher Suite: %x\n", cs)
+    fmt.Printf("Server Name: %s\n", resp.TLS.ServerName)
+    fmt.Printf("Certificate Issuer: %s\n", cert.Issuer.CommonName)
+    fmt.Printf("Certificate Subject: %s\n", cert.Subject.CommonName)
+    fmt.Printf("Valid From: %s\n", cert.NotBefore)
+    fmt.Printf("Valid Until: %s\n", cert.NotAfter)
+}
+
 func Fetch(){
+	/*TRANSPORT */
+	tr := &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		MaxIdleConns:        20,
+		IdleConnTimeout:     30 * time.Second,
+		TLSHandshakeTimeout: 10 * time.Second,
+		DisableCompression:  true,
+	}
+
+	client := &http.Client{
+    	Transport: tr,
+    	Timeout:   15 * time.Second,
+	}
+
 	for _, url := range os.Args[1:] {
 
 		url = hasHTTP(url)
 
-		response, err := http.Get(url)
+		response, err := client.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err) 
 			os.Exit(1)
@@ -28,6 +63,10 @@ func Fetch(){
 
 		b, err := io.Copy(os.Stdout, response.Body)
 		response.Body.Close()
+
+		printTLSInfo(response)
+
+
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err) 
 			os.Exit(1)
