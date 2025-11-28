e77d8ef9d6b3d4bf9110b0df1a71854b5fe7bba3
stephanie <stephanieha2021@gmail.com>
2025-11-28 14:52:33 -0500

    added response time

--- internal/fetch.go
+++ internal/fetch.go
@@ -2,54 +2,31 @@ package internal
 
 import (
 	"fmt"
-	"io"
+	//"io"
 	"net/http"
 	"os"
 	"strings"
 	"time"
+
+	"enc/pkg"
 )
 
 var (
-	MAX_IDLE = 10
+	MAX_IDLE = 2
 )
 
-func hasHTTP(url string) string {
+func HasHTTP(url string) string {
 	if !strings.HasPrefix(url, "http://") && !strings.HasPrefix(url, "https://") {
 		url = "https://" + url
 	}
 	return url
 }
 
-func printTLSInfo(resp *http.Response) {
-    if resp.TLS == nil {
-        fmt.Println("No TLS (connection was HTTP)")
-        return
-    }
-
-    cs := resp.TLS.CipherSuite
-    ver := resp.TLS.Version
-    cert := resp.TLS.PeerCertificates[0]
-
-	fmt.Printf("\n")
-	fmt.Println("--- TLS ANALYSIS ---")
-    fmt.Printf("Version: %x\n", ver)
-    fmt.Printf("Cipher Suite: %x\n", cs)
-	// Issued to..
-    fmt.Printf("Server Name: %s\n", resp.TLS.ServerName)
-	// Issued by..
-    fmt.Printf("Certificate Issuer: %s\n", cert.Issuer.CommonName)
-	fmt.Printf("Organization: %s\n", cert.Issuer.Organization)
-    fmt.Printf("Certificate Holder: %s\n", cert.Subject.CommonName)
-	// Validity period
-    fmt.Printf("Issued on: %s\n", cert.NotBefore)
-    fmt.Printf("Expires on: %s\n", cert.NotAfter)
-}
-
 func Fetch(){
 	/*TRANSPORT */
 	tr := &http.Transport{
 		Proxy: http.ProxyFromEnvironment,
-		MaxIdleConnsPerHost: 2,		
+		MaxIdleConnsPerHost: MAX_IDLE,		
 		IdleConnTimeout:     30 * time.Second,
 		TLSHandshakeTimeout: 10 * time.Second,
 		DisableCompression:  true,
@@ -61,8 +38,10 @@ func Fetch(){
 	}
 
 	for _, url := range os.Args[1:] {
+		
+		url = HasHTTP(url)
 
-		url = hasHTTP(url)
+		start := time.Now()
 
 		response, err := client.Get(url)
 		if err != nil {
@@ -70,21 +49,13 @@ func Fetch(){
 			os.Exit(1)
 		}
 
-		b, err := io.Copy(os.Stdout, response.Body)
 		response.Body.Close()
+		pkg.DisplayTLS(response)
+		
+		fmt.Printf("Response code: %s\nResponse time: %dns %fs\n", response.Status, time.Since(start).Nanoseconds(), time.Since(start).Seconds())
 
-		printTLSInfo(response)
-
-
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err) 
-			os.Exit(1)
-		}
-
-		fmt.Printf("%d\n", b)
-
-		fmt.Printf("Response code: %s\n", response.Status)
 		// CLOSE ANY idle connections, tr makes a max of 2 connections. 
 		tr.CloseIdleConnections()
 	}
+	
 }
