febce1496e1fb7b2bd4f2906553409b13ce81051
stephanie <stephanieha2021@gmail.com>
2025-11-28 18:30:13 -0500

    date formatting

--- pkg/transport.go
+++ pkg/transport.go
@@ -1,9 +1,22 @@
 package pkg
 
 import (
-	"net/http"
+	"crypto/x509"
+	"encoding/json"
 	"fmt"
-    "time"
+	"net/http"
+	"time"
+)
+
+type Date time.Time
+
+var (
+    cert        *x509.Certificate
+    duration    time.Duration
+    expiresIn   string
+    days        int
+    hours       int
+    minutes     int
 )
 
 type TLS struct {
@@ -14,27 +27,54 @@ type TLS struct {
     Issuer      string      `json:"issuer"`
     Org         []string    `json:"organization"`
     Holder      string      `json:"holder"`
-    IssuedOn    time.Time   `json:"issued_on"`
-    ExpiresOn   time.Time   `json:"expires_on"`
+    IssuedOn    Date        `json:"issued_on"`
+    ExpiresOn   Date        `json:"expires_on"`
+    ExpiresIn   string      `json:"expires_in"`
 }
 
 
-func DisplayTLS(response *http.Response) *TLS {
+func GetTLS(response *http.Response) *TLS {
     if response.TLS == nil {
         fmt.Println("No TLS (connection was HTTP)")
     }
 
-    cert := response.TLS.PeerCertificates[0]
+
+    cert = response.TLS.PeerCertificates[0]
+    
+    duration = time.Until(cert.NotAfter)
+	if duration == 0 {
+		expiresIn = "expired"
+	} else {
+		days = int(duration.Hours()) / 24
+		hours = int(duration.Hours()) % 24
+		minutes = int(duration.Minutes()) % 60
+		expiresIn = fmt.Sprintf("%d days, %d hours, %d minutes", days, hours, minutes)
+	}
 
 	return &TLS{
-		Version:      response.TLS.Version,
-		CipherSuite:  response.TLS.CipherSuite,
-		Server:   response.TLS.ServerName,
-		Issuer:       cert.Issuer.CommonName,
-		Org: cert.Issuer.Organization,
-		Holder:      cert.Subject.CommonName,
-		IssuedOn:    cert.NotBefore,
-		ExpiresOn:   cert.NotAfter,
+		Version:        response.TLS.Version,
+		CipherSuite:    response.TLS.CipherSuite,
+		Server:         response.TLS.ServerName,
+		Issuer:         cert.Issuer.CommonName,
+		Org:            cert.Issuer.Organization,
+		Holder:         cert.Subject.CommonName,
+		IssuedOn:       Date(cert.NotBefore),
+		ExpiresOn:      Date(cert.NotAfter),
+        ExpiresIn:      expiresIn,
 	}
 }
+
+func (t *TLS) JSON() (string, error) {
+    b, err := json.MarshalIndent(t, "", "   ") 
+    if err != nil {
+        return "", err
+    }
+    return string(b), nil
+}
+
+func (d Date) MarshalJSON() ([]byte, error) {
+    str := time.Time(d).In(time.Local).Format("Mon Jan 2 15:04:05 MST 2006")
+    return []byte(`"` + str + `"`), nil
+}
+
 
